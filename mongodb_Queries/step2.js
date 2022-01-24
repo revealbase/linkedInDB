a814a15ec013040b0cf677dc0a670c0d60b3e7d6
revealbase <98247506+revealbase@users.noreply.github.com>
2022-01-23 20:54:18 -0500

    Create step2.js

---
+++ mongodb_Queries/step2.js
@@ -0,0 +1,174 @@
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$certifications",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "certifications.id": "$_id", "certifications.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$certifications" } },
+	{ "$out": "certifications" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$courses",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "courses.id": "$_id", "courses.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$courses" } },
+	{ "$out": "courses" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$educations",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "educations.id": "$_id", "educations.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$educations" } },
+	{ "$out": "educations" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$honors",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "honors.id": "$_id", "honors.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$honors" } },
+	{ "$out": "honors" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$languages",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "languages.id": "$_id", "languages.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$languages" } },
+	{ "$out": "languages" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$organizations",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "organizations.id": "$_id", "organizations.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$organizations" } },
+	{ "$out": "organizations" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$patents",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "patents.id": "$_id", "patents.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$patents" } },
+	{ "$out": "patents" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$positionGroups",
+			"includeArrayIndex": "indexName",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "positionGroups.profilePositionInPositionGroup._id": "$_id" } },
+	{ "$replaceRoot": { "newRoot": "$positionGroups.profilePositionInPositionGroup" } },
+	{
+		"$unwind": {
+			"path": "$elements",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "elements.id": "$_id", "elements.index0": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$elements" } },
+	{ "$out": "positions" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$positionGroups",
+			"includeArrayIndex": "indexName",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "positionGroups.profilePositionInPositionGroup._id": "$_id" } },
+	{ "$replaceRoot": { "newRoot": "$positionGroups.profilePositionInPositionGroup" } },
+	{
+		"$unwind": {
+			"path": "$elements",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "elements.id": "$_id", "elements.index0": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$elements" } },
+	{ "$out": "positions" }
+]);
+b.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$projects",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "projects.id": "$_id", "projects.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$projects" } },
+	{ "$out": "projects" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$skills",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "skills.id": "$_id", "skills.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$skills" } },
+	{ "$out": "skills" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$testScores",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "testScores.id": "$_id", "testScores.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$testScores" } },
+	{ "$out": "testScores" }
+]);
+db.getCollection("step1").aggregate([
+	{
+		"$unwind": {
+			"path": "$volunteerExperiences",
+			"includeArrayIndex": "index",
+			"preserveNullAndEmptyArrays": false
+		}
+	},
+	{ "$addFields": { "volunteerExperiences.id": "$_id", "volunteerExperiences.index": "$index" } },
+	{ "$replaceRoot": { "newRoot": "$volunteerExperiences" } },
+	{ "$out": "volunteerExperiences" }
+])
