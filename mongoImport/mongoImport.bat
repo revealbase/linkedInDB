cfdd1b883480ff378b9f9aab7127e6d28e3b46a3
revealbase <98247506+revealbase@users.noreply.github.com>
2022-01-23 20:55:10 -0500

    Update mongoImport.bat

--- mongoImport/mongoImport.bat
+++ mongoImport/mongoImport.bat
@@ -1,1 +1,1 @@
-mongoimport -v --host localhost:27017 --file D:\sample.json -d "linkedin" -c "sample" --type json
+mongoimport -v --host localhost:27017 --file sample.json -d "linkedin" -c "sample" --type json
